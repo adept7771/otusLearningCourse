129c463f8ce04c1c0f00dce54d63708b236a2b54
dmitrijpotapov <dmitry.potapov@btsdigital.kz>
2020-07-12 17:06:00 +0600

    initial

---
+++ src/test/java/FirstLessonTests.java
@@ -0,0 +1,39 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstLessonTests {
+
+    protected static WebDriver driver;
+    private Logger logger = LogManager.getLogger(FirstLessonTests.class);
+
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        logger.info("Драйвер поднят");
+    }
+
+    @Test
+    public void firstTest(){
+        driver.get("https://otus.ru");
+        logger.info("Открыта страница отус");
+        Assert.assertTrue("Тайтл не содержит заданный текст.",
+                driver.getTitle().contains(
+                        "Онлайн‑курсы для профессионалов, дистанционное обучение современным профессиям"));
+    }
+
+    @After
+    public void setDown(){
+        if(driver != null){
+            logger.info("Драйвер выключен");
+            driver.quit();
+        }
+    }
+}
