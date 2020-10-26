6bd1c8cca4ae104deed9115072072c20820a3853
Dmitry <metallfun@gmail.com>
2020-10-26 14:13:07 +0300

    test develop

--- src/test/java/steps/ApiSteps.java
+++ src/test/java/steps/ApiSteps.java
@@ -6,18 +6,30 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.response.Response;
 import org.junit.Assert;
-import pages.MainPage;
 import services.UserService;
+import utils.Generators;
 
 public class ApiSteps extends TestsCore {
 
     Response response;
+    String generatedUserName;
 
     @When("I sent post user request with valid data to pet store")
     public void sentCreateUserRequest() {
         UserService userService = new UserService();
         response = userService.addUserRequest(User.builder().email("test").username("testname").lastName("lastname")
         .password("123").phone("22222").id(100L).firstName("FirstName").userStatus(10L).build());
+    }
+
+    @When("I sent post user request with data and random name: {string}, {string}, {string}, {string}, {string}")
+    public void sentCreateUserRequestWithPredefinedParameters(String firstName, String lastName,
+                                                              String email,
+                                                              String password, String phone) {
+        UserService userService = new UserService();
+        generatedUserName = Generators.genString(6, false, true, false,
+                false, false);
+        response = userService.addUserRequest(User.builder().email(email).username(generatedUserName).lastName(lastName)
+                .password(password).phone(phone).id(100L).firstName(firstName).userStatus(10L).build());
     }
 
     @When("I sent post user request with valid data to pet store with custom base api path {string}")
@@ -33,27 +45,26 @@ public class ApiSteps extends TestsCore {
         response = userService.getUserByName(username);
     }
 
+    @When("I get pre generated user")
+    public void getPreGeneratedUser() {
+        UserService userService = new UserService();
+        response = userService.getUserByName(generatedUserName);
+    }
+
     @Then("Server response code is {int}")
     public void serverResponseCodeIs(int responseCode) {
         Assert.assertEquals(responseCode, response.getStatusCode());
     }
 
-    @Then("User in response contains data:")
-//    public void getUserRequestContainsData(String username, String firstName, String lastName, String email,
-//                                           String password, String phone) {
-
-    public void getUserRequestContainsData() {
-
+    @Then("User with random name contains data in response: {string}, {string}, {string}, {string}, {string}")
+    public void getUserRequestContainsData(String firstName, String lastName, String email,
+                                           String password, String phone) {
         User user = response.as(User.class);
-//        Assert.assertEquals(user.getUsername(), username);
-//        Assert.assertEquals(user.getFirstName(), firstName);
-//        Assert.assertEquals(user.getLastName(), lastName);
-//        Assert.assertEquals(user.getEmail(), email);
-//        Assert.assertEquals(user.getUsername(), username);
-//        Assert.assertEquals(user.getUsername(), username);
-//        Assert.assertEquals(user.getUsername(), username);
-//        Assert.assertEquals(user.getUsername(), username);
-
-        System.out.println("test");
+        Assert.assertEquals(user.getUsername(), generatedUserName);
+        Assert.assertEquals(user.getFirstName(), firstName);
+        Assert.assertEquals(user.getLastName(), lastName);
+        Assert.assertEquals(user.getEmail(), email);
+        Assert.assertEquals(user.getPassword(), password);
+        Assert.assertEquals(user.getPhone(), phone);
     }
 }
--- src/test/resources/features/apiTests.feature
+++ src/test/resources/features/apiTests.feature
@@ -6,11 +6,16 @@ Feature: Validate Otus statistic
     Given I sent post user request with valid data to pet store
     Then Server response code is 200
 
-  Scenario: I cant create single user with invalid data and get error response code
+  Scenario: I can't create single user with invalid data and get error response code
     Given I sent post user request with valid data to pet store with custom base api path 'https://petstore.swagger.io/wrong'
     Then Server response code is 404
 
-  Scenario: I can get user with valid name
-    Given I get user by username: 'user'
+  Scenario: I can get user with pre generated random user name
+    Given I sent post user request with data and random name: 'firstName', 'lastName', 'email@mail.ru', 'pass', 'phone'
+    When I get pre generated user
     Then Server response code is 200
-    Then User in response contains data:
+    Then User with random name contains data in response: 'firstName', 'lastName', 'email@mail.ru', 'pass', 'phone'
+
+  Scenario: I can't get user with non-existing user name
+    When I get user by username: 'nonExistedUserForTests'
+    Then Server response code is 404
