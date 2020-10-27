9cc194f78368b8bcd2db82577f034b99ed0d5ad1
Dmitry <metallfun@gmail.com>
2020-10-27 14:00:54 +0300

    test develop

--- src/test/java/core/DriverManager.java
+++ src/test/java/core/DriverManager.java
@@ -5,26 +5,40 @@ import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
 
 public class DriverManager {
 
     private static Logger logger = LogManager.getLogger(DriverManager.class);
     private static WebDriver webDriver;
 
+    @lombok.SneakyThrows
     public static void setupDriver() {
-        String browserNameFromSystem = null;
-        try {
-            browserNameFromSystem = System.getProperty("browser");
-            if (browserNameFromSystem == null) {
-                browserNameFromSystem = "chrome";
-            }
-        } catch (Exception e) {
-            browserNameFromSystem = "chrome";
-        }
-        webDriver = WebDriverFactory.create(browserNameFromSystem);
+//        String browserNameFromSystem = null;
+//        try {
+//            browserNameFromSystem = System.getProperty("browser");
+//            if (browserNameFromSystem == null) {
+//                browserNameFromSystem = "chrome";
+//            }
+//        } catch (Exception e) {
+//            browserNameFromSystem = "chrome";
+//        }
+//        webDriver = WebDriverFactory.create(browserNameFromSystem);
 //        Point point = new Point(-1380, 700); // open browser on second screen
 //        webDriver.manage().window().setPosition(point);
 //        webDriver.manage().window().setSize(new Dimension(1390, 810));
+        String slenoidURL = "http://localhost:4444/wd/hub";
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setBrowserName("chrome");
+        caps.setVersion("86.0");
+        caps.setCapability("enableVNC", true);
+        caps.setCapability("screenResolution", "1280x1024");
+        caps.setCapability("enableVideo", true);
+        caps.setCapability("enableLog", true);
+        webDriver = new RemoteWebDriver(new URL(slenoidURL), caps);
         logger.info("Драйвер поднят");
     }
 
